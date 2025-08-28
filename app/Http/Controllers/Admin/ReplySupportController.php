4d513f4c44230f3d4a21633894f180988b1f2725
Eleandro Lima <eleandro.lima@treinacon.net>
2025-08-28 16:39:14 -0300

    feat: get all replies of support

--- app/Http/Controllers/Admin/ReplySupportController.php
+++ app/Http/Controllers/Admin/ReplySupportController.php
@@ -3,21 +3,27 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Services\ReplySupportService;
 use App\Services\SupportService;
 use Illuminate\Http\Request;
 
 class ReplySupportController extends Controller
 {
     public function __construct(
-        protected SupportService $service
-    ) {}
-    
+        protected SupportService $supportService,
+        protected ReplySupportService $replyService,
+    ) {
+    }
+
     public function index(string $id)
     {
-        if (!$replies = $this->service->findOne($id)) {
+        if (!$support = $this->supportService->findOne($id)) {
             return back();
         }
 
-        return view('admin.supports.replies.replies', compact('replies'));
+        $replies = $this->replyService->getAllBySupportId($id);
+
+        return view('admin.supports.replies.replies', compact('support', 'replies'));
     }
+
 }
