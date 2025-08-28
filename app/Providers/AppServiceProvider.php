170c04437bfb14a1672779b423ff0a28092c4184
Eleandro Lima <eleandro.lima@treinacon.net>
2025-08-28 16:34:56 -0300

    feat: bind repository with interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use App\Models\Support;
 use App\Observers\SupportObserver;
 use App\Repositories\{SupportEloquentORM};
-use App\Repositories\Contracts\{SupportRepositoryInterface};
+use App\Repositories\Contracts\{ReplyRepositoryInterface, SupportRepositoryInterface};
 use App\Repositories\Eloquent\ReplySupportRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -19,6 +19,11 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             SupportRepositoryInterface::class,
             SupportEloquentORM::class
+        );
+
+        $this->app->bind(
+            ReplyRepositoryInterface::class,
+            ReplySupportRepository::class
         );
     }
 
