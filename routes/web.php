cc002773ffef3ebbf4d0b9897db1b383bf43117d
Eleandro Lima <eleandro.lima@treinacon.net>
2025-08-28 13:28:29 -0300

    refactor: create new module for replies of support

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\Admin\{ReplySupportController, SupportController};
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,12 +29,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/supports/{id}/replies', [ReplySupportController::class, 'index'])->name('replies.index');
 
     Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
     Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
     Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
     Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-    Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
     Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
     Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 });
