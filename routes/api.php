a972fdaa72ddeab75090edf983117cd1d16981b4
Eleandro Lima <eleandro.lima@treinacon.net>
2025-08-29 18:22:09 -0300

    feat: create route list all replies of support

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\ReplySupportApiController;
 use App\Http\Controllers\Api\SupportController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,10 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/replies/{support_id}', [ReplySupportApiController::class, 'getRepliesFromSupport']);
+    Route::post('/replies/{support_id}', [ReplySupportApiController::class, 'createNewReply']);
+    Route::delete('/replies/{id}', [ReplySupportApiController::class, 'destroy']);
+
     Route::apiResource('/supports', SupportController::class);
 });
-
+// Route::apiResource('/supports', SupportController::class)->middleware('auth');
